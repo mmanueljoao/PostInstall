baea36e60066c26293c3a3c19d9b2688538aea0b
mmanueljoao <geral@mjoaolima.eu>
2021-03-08 19:05:17 +0000

    Bug fixes: - Fixed aliases and zshrc file creation. Now using *cp* instead of *echo*; - Added silent install for startship; - Fixed permitions for *ln* of rust core-utils.

--- PostInstall.bash
+++ PostInstall.bash
@@ -18,53 +18,51 @@ sudo apt update &>/dev/null && sudo apt list --upgradable && sudo apt -y full-up
 
 # apt install
 echo
-printf "Installing needed packages... "
-sudo apt install -y vim curl git &>/dev/null
+printf "Installing vim, cargo, curl, git, net-tools and lynx... "
+sudo apt install -y vim cargo curl git net-tools lynx &>/dev/null && echo "Completed"
+echo
 
+printf "Changing git config --global user values... "
 git config --global init.defaultBranch main
 git config --global user.email "geral@mjoaolima.eu"
 git config --global user.name "mmanueljoao" && echo "Completed"
 
-echo
-printf "Installing net-tools, vim and lynx ... "
-sudo apt install -y net-tools vim lynx &>/dev/null && echo "Completed"
+# # LAMP Install
+# echo
+# printf "Installing Apache2 ... "
+# sudo apt install -y apache2 &>/dev/null && echo "Completed"
+# apache2 -v
+# echo
 
-# LAMP Install
-echo
-printf "Installing Apache2 ... "
-sudo apt install -y apache2 &>/dev/null && echo "Completed"
-apache2 -v
-echo
+# printf "Installing MySQL ... "
+# sudo apt install -y mysql-server &>/dev/null && echo "Completed"
+# mysql -V
+# echo
 
-printf "Installing MySQL ... "
-sudo apt install -y mysql-server &>/dev/null && echo "Completed"
-mysql -V
-echo
+# printf "Installing PHP ... "
+# sudo apt install -y php libapache2-mod-php php-mysql &>/dev/null && echo "Completed"
+# php -v
+# echo "    Installed Modules printed to file - php -m"
+# touch $HOME/php_installed_modules
+# php -m >>php_installed_modules
+# echo
 
-printf "Installing PHP ... "
-sudo apt install -y php libapache2-mod-php php-mysql &>/dev/null && echo "Completed"
-php -v
-echo "    Installed Modules printed to file - php -m"
-touch $HOME/php_installed_modules
-php -m >>php_installed_modules
-echo
-
-echo "Current IP Address:"
-printf "    " && curl ifconfig.me
-echo && echo
+# echo "Current IP Address:"
+# printf "    " && curl ifconfig.me
+# echo && echo
 
 # Installing ZSH
 printf "Installing ZSH ..."
 sudo apt install -y zsh && echo " Completed"
 printf "Changing User Shell ..."
-usermod --shell /bin/zsh $USERNAME && echo " Completed"
+sudo usermod --shell /bin/zsh $USERNAME && echo " Completed"
 echo "Installing OH-MY-ZSH ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && echo " Completed"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions &>/dev/null
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
 
-echo ./zshrc >$HOME/.zshrc
+cp ./zshrc $HOME/.zshrc
 echo
 
 # Creating .aliases
@@ -75,12 +73,12 @@ ALIASES="$HOME/.aliases"
 echo
 if [ -f "$ALIASES" ]; then
 	printf "    ~/.aliases already exists. Moving to ~/.aliases.bak\n"
-	sudo mv $ALIASES $ALIASES.bak
+	mv $ALIASES $ALIASES.bak
 fi
 
 touch $ALIASES
 echo
-echo ./aliases >$ALIASES
+cp ./aliases $ALIASES
 source $ALIASES
 echo -e "    Check your new ~/.aliases at $ALIASES.\t"
 echo
@@ -88,13 +86,13 @@ echo
 # Starship - Cross Shell Prompt
 echo "Preparing for take-off - https://starship.rs/"
 printf "Installing Starship ..."
-curl -fsSL https://starship.rs/install.sh -y | bash &>/dev/null
+curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
 
-mkdir $HOME/.config
-echo ./starship >$HOME/.config/starship.toml && echo " Completed"
+mkdir -p $HOME/.config
+cp ./starship $HOME/.config/starship.toml && echo " Completed"
 
-printf "De-Rusting utilities ..."
-sudo apt install -y cargo exa bat ripgrep fd-find
-cargo install procs && ln -s $HOME/.cargo/bin/procs /usr/local/bin && echo " Completed"
+printf "Rusting core-utils ..."
+sudo apt install -y exa bat ripgrep fd-find
+cargo install procs && sudo ln -s $HOME/.cargo/bin/procs /usr/local/bin && echo " Completed"
 
 echo "Post Install Script is finished."
